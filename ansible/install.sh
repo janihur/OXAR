16d7a872b6cceb77f524d7f9e5ff264855489a7d
Jani Hur <webmaster@jani-hur.net>
2016-04-16 21:26:17 +0300

    Skip ansible install if already available.

--- ansible/install.sh
+++ ansible/install.sh
@@ -4,8 +4,13 @@
 # Install only a specific version so that updates don't break the provisioning scripts.
 # TODO: other distros too currently centos 7.2 only
 
-yum install -y epel-release
-# TODO: version hardcoded
-yum install -y ansible-${1}-1.el7
-# TODO: this is the wrong place, move to ansible playbook
-# yum --exclude=ansible\* --exclude=kernel\* update -y
+command -v ansible > /dev/null
+
+if (($? == 1))
+then
+    yum install -y epel-release
+    # TODO: version hardcoded
+    yum install -y ansible-${1}-1.el7
+    # TODO: this is the wrong place, move to ansible playbook
+    # yum --exclude=ansible\* --exclude=kernel\* update -y
+fi
